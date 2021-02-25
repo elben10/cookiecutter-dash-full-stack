6d3ba44b884ac08ee0039d3473a8939b7a730e27
Jakob Jul Elben <elbenjakobjul@gmail.com>
2021-02-25 15:41:54 +0100

    Fix linting error

--- {{cookiecutter.project_slug}}/app/app.py
+++ {{cookiecutter.project_slug}}/app/app.py
@@ -11,7 +11,7 @@ CSS = [
 ]
 JS = [
     "https://code.jquery.com/jquery-3.5.1.slim.min.js",
-    "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js",
+    "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js",  # noqa: E501
 ]
 
 app = Dash(
